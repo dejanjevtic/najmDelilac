a59ec9c7e81b57fc6d373dbb233dbf373cade8fe
dejanjevtic <dejan_d_jevtic@yahoo.com>
2021-06-10 11:25:13 +0200

    pozivanje funkcije printDivisors

--- src/najmdelilac/NajmDelilac.java
+++ src/najmdelilac/NajmDelilac.java
@@ -15,7 +15,8 @@ public class NajmDelilac {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+         System.out.println("The divisors of 24 are: ");
+         printDivisors(24);
     }
     
 }
