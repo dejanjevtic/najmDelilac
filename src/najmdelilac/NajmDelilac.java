676717f4fd13d56fac8cbf79be791d46d1449e8b
dejanjevtic <dejan_d_jevtic@yahoo.com>
2021-06-10 11:26:35 +0200

    funkcija printDivisors

--- src/najmdelilac/NajmDelilac.java
+++ src/najmdelilac/NajmDelilac.java
@@ -5,12 +5,18 @@
  */
 package najmdelilac;
 
+
 /**
  *
  * @author admin
  */
 public class NajmDelilac {
-
+    static void printDivisors(int n)
+    {
+        for (int i=1;i<=n;i++)
+            if (n%i==0)
+                System.out.print(i+" ");
+    }
     /**
      * @param args the command line arguments
      */
